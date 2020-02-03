d848a6d7af96bbeee70b3bdfb33fd6be8fb2b462
nitishpratap <54417889+nitishpratap@users.noreply.github.com>
2020-02-03 21:14:59 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[20],b[20],alice=0,bob=0;
+    scanf("%d %d %d",&a[0],&a[1],&a[2]);
+    scanf("%d %d %d",&b[0],&b[1],&b[2]);
+    if(a[0]>b[0]&&)
+        alice++;
+    else
+        bob++;
+    if(a[1]>b[1])
+        alice++;
+    else
+        bob++;
+    if(a[2]>b[2])
+        alice++;
+    else
+        bob++;
+   printf("%d %d",alice,bob);
+    return 0;
+}
